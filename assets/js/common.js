f1216c1cc0df82e42f67fac01f98e76034836a93
marcecabelloherrera <marcecabello.h@gmail.com>
2017-07-29 20:04:36 -0400

    estilos sass modal

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,31 +1,16 @@
 $(document).ready(function(){
-	//función que llama a masonry, página que permite desplegar imagenes una abajo de la otra.
-	$('.contenedor').masonry({
-  		itemSelector: '.elemento',
-	});
+
 
 //llamo a mi json
 	jason.forEach(function(e){
-		console.log(e.title);
-		var titulo = e.title;
-		var description = e.description;
-		for(var i=0; i < e; i++){
-			var elemento = e[i];
-			console.log(elemento);
-			$('.elemento').append('<p class="titulo">'+i.title+'</p>');
-		}
+		$('.elemento').append('<img class="materialboxed modal-trigger" href="#modal1" src="dist/img/'+ e.image_url +'">'+ '<br>' + '<i class="fa fa-check grey-text right" aria-hidden="true"> 6</i>'+'<i class="fa fa-thumb-tack grey-text right" aria-hidden="true"> 36,6k</i>'+'<h6 class="titulo left-align">'+ e.title+'</h6><p class= "descripcion left-align">'+ e.description+'</p><p class= "user left-align">'+ e.user + '&nbsp<span>#</span>' + e.hashtag + '</p>');	
 	})
 
-/*
-('https://api.dribbble.com/v1/users/wellscollins/shots?access_token=4644069fd6dfeba94b66e75c1b47e088f3444120a121ba0b481f9e7637a72ccb&callback=?', function(resp) {
-      if (resp.data.length > 0) {
-      console.log(resp);            
-        $.each(resp.data.reverse(), function(i, val) {
-        $('.project').prepend('<a href='+val.html_url+'><div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 box-project"><img class="img-responsive" src="'+val.images.normal+'"/><div class="overlay"><div class="text">'+ val.description+'</div></div><span><i class="fa fa-eye" aria-hidden="true"></i>'+val.views_count+'</span><span><i class="fa fa-comment-o" aria-hidden="true"></i>'+val.comments_count+'<i class="fa fa-heart" aria-hidden="true"></i>'+val.likes_count+'</span></div></a>')
-      });
-      }else{
-          $('.project').append('<li>No shots.</li>');
-        }
-      });                       
-  });*/
-})
+	jason.forEach(function(e){
+		$('.contenido-modal').append("<h6 class='titulo left-align'>"+ e.title+"</h6>"+"<br>"+ "<img src='dist/img/"+ e.id +".jpg'>" +"<div class='row'>"+"<a href='#'><i class='fa fa-upload' aria-hidden='true'></i></a>" +"<a href='#'><i class='fa fa-check' aria-hidden='true'></i></a>"
+			+"<a href='#'><i class='fa fa-ellipsis-h' aria-hidden='true'></i></a>"+"<a class='btn red right'><i class='fa fa-thumb-tack' aria-hidden='true'></i> Guardar</a>"+"</div>"+"</div>" + "<div class='row'>"+"<div class='col m6 l6 xl6'>"+"<p class='user left-align'>"+ e.user +"&nbsp<span>#</span>"+e.hashtag + "</p>"+"</div>"+"<div class='col m6 l6 xl6'>"+"<a class='btn btn-leerlo right'>Leerlo</a>"+"</div>"+"</div>"+"<p class='descripcion left-align'>"+e.description+"</p>");	
+	})
+
+
+ $('.modal').modal();
+});
